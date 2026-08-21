878d0e63ef83a2dd90523e01fe0c41d23d7d337e
rootankush <rootankushprompt@gmail.com>
2026-08-21 22:01:31 +0530

    feat: added describtion of the game

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,9 @@ export default function Header({ currentScore, highScore }) {
   return (
     <header>
       <h1>Memory Card Game</h1>
+      <hr />
+      <h1 className="describtion">click each card once, don't repeat</h1>
+      <h1 className="describtion">scroll for more cards</h1>
       <div className="score-card">
         <h1>High Score:{highScore}</h1>
         <h1>Score:{currentScore}</h1>
