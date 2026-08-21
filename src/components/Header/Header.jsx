31870c565509f6488f2e1642be38c78343bc7905
rootankush <rootankushprompt@gmail.com>
2026-08-21 19:40:31 +0530

    feat: added currentScore, highScore props and media querie for different screen

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 import "./Header.css";
 
-export default function Header() {
+export default function Header({ currentScore, highScore }) {
   return (
     <header>
       <h1>Memory Card Game</h1>
       <div className="score-card">
-        <h1>High Score:</h1>
-        <h1>Score:</h1>
+        <h1>High Score:{highScore}</h1>
+        <h1>Score:{currentScore}</h1>
       </div>
     </header>
   );
