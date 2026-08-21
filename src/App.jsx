55011381c0b6dab5343dec31857f25d03c778bf6
rootankush <rootankushprompt@gmail.com>
2026-08-21 08:05:00 +0530

    chore: added img files and fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import { useState } from "react";
+import Gameboard from "./components/Gameboard/Gameboard";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header></Header>
+      <Gameboard></Gameboard>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Gameboard/Gameboard.jsx
@@ -0,0 +1,6 @@
+import Card from "../Card/Card.jsx";
+import "../Gameboard/Gameboard.css";
+
+export default function Gameboard() {
+  return <main></main>;
+}
